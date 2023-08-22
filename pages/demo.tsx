6d5312022787c383e06aec0645946538af4328e5
Moisés Gamero <moises1109@gmail.com>
2023-08-21 21:49:28 -0500

    Imagen ok

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { getPersonas } from './api/strapi';
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import { getPersonas } from "./api/strapi";
+import Image from "next/image";
 interface persona {
   id: number;
   attributes: {
@@ -9,21 +9,21 @@ interface persona {
     Correo: string;
     Foto?: {
       data: {
-        formats: {
-          small: {
-            url: string;
+        attributes: {
+          formats: {
+            small: {
+              url: string;
+            };
+            medium: {
+              url: string;
+            };
+            // Agrega otros formatos aquí si es necesario
           };
-          medium: {
-            url: string;
-          };
-          // Agrega otros formatos aquí si es necesario
         };
       };
     };
   };
 }
-
-
 
 function Demo() {
   const [personas, setpersonas] = useState<persona[]>([]);
@@ -46,24 +46,30 @@ function Demo() {
   }, []);
 
   return (
-    <div className='h-screen p-8'>
-      <h1 className='text-3xl font-bold mb-4'>Recopilar Datos de Strapi</h1>
+    <div className="h-screen p-8">
+      <h1 className="text-3xl font-bold mb-4">Recopilar Datos de Strapi</h1>
       {loading ? (
         <p>Cargando...</p>
       ) : error ? (
-        <p className='text-red-500'>Error: {error.message}</p>
+        <p className="text-red-500">Error: {error.message}</p>
       ) : (
         <div>
-          <p className='mb-4'>Estos datos vienen de Strapi:</p>
-          <ul className='space-y-4'>
-            {personas.map(persona => (
-              <li key={persona.id} className='border p-4 rounded shadow'>
-                <h2 className='text-xl font-semibold'>{persona.attributes.Nombre}</h2>
-                <p className='mt-2 text-gray-600'>{persona.attributes.Apellido}</p>
-                <p className='mt-1 text-sm text-gray-500'>Correo: {persona.attributes.Correo}</p>
+          <p className="mb-4">Estos datos vienen de Strapi:</p>
+          <ul className="space-y-4">
+            {personas.map((persona) => (
+              <li key={persona.id} className="border p-4 rounded shadow">
+                <h2 className="text-xl font-semibold">
+                  {persona.attributes.Nombre}
+                </h2>
+                <p className="mt-2 text-gray-600">
+                  {persona.attributes.Apellido}
+                </p>
+                <p className="mt-1 text-sm text-gray-500">
+                  Correo: {persona.attributes.Correo}
+                </p>
                 {/* Mostrar la imagen si está disponible */}
                 {persona.attributes.Foto && persona.attributes.Foto.data && (
-                  <Image  
+                  <Image
                     src={`http://147.182.243.95:1337${persona.attributes.Foto.data.attributes.formats.small.url}`}
                     alt={`${persona.attributes.Nombre} ${persona.attributes.Apellido}`}
                     width={300}
@@ -77,7 +83,6 @@ function Demo() {
       )}
     </div>
   );
-  
 }
 
 export default Demo;
